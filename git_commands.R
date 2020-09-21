cec9499b39626f5d6316a00e11749ff9f8a1d309
minju lee <minju.lee@wsu.edu>
2020-09-21 15:58:11 -0700

    functions

---
+++ git_commands.R
@@ -0,0 +1,25 @@
+Git commands
+
+setting up ssh keys
+
+ssh-keygen -t rsa -b 4096 -C "mlee09206@gmail.com"
+
+git clone
+https://github.com/minju-lee92/WSU_STATS419_FALL2020.git
+-------------------------------------------------------------
+# make directory (Folder)
+mkdir 
+
+# create enpty file
+touch 
+
+## make changes tot he folder ... 
+git add .
+git status
+git commit  -m "Adding personality-raw"
+git push -u origin master
+
+## set remote url to know where we are pushing it to
+
+git remote set-url origin
+git@github.com:minju-lee92/WSU_STATS419_FALL2020.git
