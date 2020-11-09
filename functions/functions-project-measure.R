4fbff8808cc4499e823e6d04e1e4c67a711b645e
minju lee <mlee09206@gmail.com>
2020-11-09 04:04:30 -0800

    added prepareMeasureData()

--- functions/functions-project-measure.R
+++ functions/functions-project-measure.R
@@ -90,3 +90,121 @@ build.scale.variables = function(df, colnum, new.colname, scalevar )
   }
   df
 }
+
+prepareMeasureData = function(df)
+{
+  measure <-df
+  nrow = nrow(measure);
+  ncol = ncol(measure);
+  # clean up the variable names
+  myUnits = tolower(measure$units);
+  myUnits[myUnits=="inches"] = "in";
+  myUnits[myUnits=="inch"] = "in";
+  myUnits[myUnits=="\"in\""] = "in";
+  measure$my.units = myUnits;
+  myEthnicity = tolower(measure$ethnicity);
+  myEthnicity[myEthnicity=="white"] = "w";
+  myEthnicity[myEthnicity=="caucasian"] = "w";
+  myEthnicity[myEthnicity=="caucasain"] = "w";
+  myEthnicity[myEthnicity=="\"caucasian\""] = "w";
+  myEthnicity[myEthnicity=="anglo"] = "w";
+  myEthnicity[myEthnicity=="white non-hispanic"] = "w";
+  myEthnicity[myEthnicity=="black"] = "b";
+  myEthnicity[myEthnicity=="african american"] = "b";
+  myEthnicity[myEthnicity=="hispanic"] = "h";
+  myEthnicity[myEthnicity=="japanese"] = "a";
+  myEthnicity[myEthnicity=="filipino"] = "pi";
+  myEthnicity[myEthnicity=="native american"] = "nat";
+  myEthnicity[myEthnicity=="latin american"] = "l";
+  myEthnicity[myEthnicity=="latino"] = "l";
+  myEthnicity[myEthnicity=="\"asian/latino\""] = "al";
+  myEthnicity[myEthnicity=="white-filipino"] = "wf";
+  myEthnicity[myEthnicity=="asain"] = "a";
+  myEthnicity[myEthnicity=="asian"] = "a";
+  myEthnicity[myEthnicity=="\"asian\""] = "a";
+  myEthnicity[myEthnicity=="chinese"] = "a";
+  myEthnicity[myEthnicity=="laotian"] = "a";
+  myEthnicity[myEthnicity=="caucasian/asian"] = "ca";
+  myEthnicity[myEthnicity=="white italian"] = "w";
+  myEthnicity[myEthnicity=="japanese italian"] = "ji";
+  myEthnicity[myEthnicity=="pacific islander"] = "pi";
+  myEthnicity[myEthnicity=="korean"] = "a";
+  myEthnicity[myEthnicity=="indian"] = "a";
+  measure$my.ethnicity = myEthnicity;
+  myGender = tolower(measure$gender);
+  myGender[myGender=="female"] = "f";
+  myGender[myGender=="male"] = "m";
+  myGender[myGender=="\"male\""] = "m";
+  myGender[myGender=="\"female\""] = "f";
+  myGender[myGender=="non-binary"] = "o";
+  measure$my.gender = myGender;
+  
+  measure$new.units = "cm";
+############################################################
+  # Save measue rds 
+  saveRDS(measure,"measure.rds");
+  measure.short = measure[,c(1,2,4:6,17,24,27:50)];
+  myNames = gsub(".NA","", colnames(measure.short), fixed=TRUE);
+  colnames(measure.short) = myNames;
+  colnames(measure.short)[11] = "eye.color";
+  # get rid of duplicated tuples and save as rds
+  measure.df = removeDuplicatesFromDataFrameAllColumns(measure.short);
+  saveRDS(measure.df,"measure.df.rds");
+  
+  # create final df, delete rows for specific collector and person_id for the data integrity 
+  final.df = measure.df;
+  final.df = subsetDataFrame(final.df, "data_collector", "!=", "411f8b5c9500b7cc928c93dd1a0006b7");
+  final.df = subsetDataFrame(final.df, "person_id", "!=", "81a9f3915f64dc6edd329b89bea87d5e");
+  final.df = subsetDataFrame(final.df, "person_id", "!=", "\"ee33e909372d935d190f4fcb2a92d542\"");
+  
+  # rename values and categorize certain attributes
+  myEye = tolower(final.df$eye);
+  myEye[myEye == "both"] = "b";
+  myEye[myEye == "\"both\""] = "b";
+  myEye[myEye == "equal"] = "b";
+  myEye[myEye == "right"] = "r";
+  myEye[myEye == "\"right\""] = "r";
+  myEye[myEye == "left"] = "l";
+  myEye[myEye == "\"left\""] = "r";
+  myEye[myEye == "brown"] = NA;
+  final.df$my.eye = factor(myEye);
+  myWriting = tolower(final.df$writing);
+  myWriting[myWriting == "right"] = "r";
+  myWriting[myWriting == "\"right\""] = "r";
+  myWriting[myWriting == "left"] = "l";
+  myWriting[myWriting == "both"] = "b";
+  final.df$my.writing = factor(myWriting);
+  mySwinging = tolower(final.df$swinging);
+  mySwinging[mySwinging == "right"] = "r";
+  mySwinging[mySwinging == "rigth"] = "r";
+  mySwinging[mySwinging == "\"right\""] = "r";
+  mySwinging[mySwinging == "left"] = "l";
+  mySwinging[mySwinging == "let"] = "l";
+  mySwinging[mySwinging == "leftt"] = "l";
+  mySwinging[mySwinging == "both"] = "b";
+  final.df$my.swinging = factor(mySwinging);
+  myEyeColor = tolower(final.df$eye.color);
+  myEyeColor[myEyeColor == "brown"] = "br";
+  myEyeColor[myEyeColor == "\"brown\""] = "br";
+  myEyeColor[myEyeColor == "blue"] = "bl";
+  myEyeColor[myEyeColor == "\"blue\""] = "bl";
+  myEyeColor[myEyeColor == "green"] = "gr";
+  myEyeColor[myEyeColor == "hazel"] = "ha";
+  myEyeColor[myEyeColor == "grey"] = "g";
+  myEyeColor[myEyeColor == "blue-green"] = "bl-gr";
+  myEyeColor[myEyeColor == "blue/green"] = "bl-gr";
+  myEyeColor[myEyeColor == "blue-grey"] = "bl-g";
+  myEyeColor[myEyeColor == "grey-green"] = "gr-g";
+  myEyeColor[myEyeColor == "black"] = "b";
+  myEyeColor[myEyeColor == "left"] = NA;
+  final.df$my.eye.color = factor(myEyeColor);
+  final.df$my.gender = factor(final.df$my.gender);
+  final.df$my.ethnicity = factor(final.df$my.ethnicity);
+  # measure.nic = subsetDataFrame(measure.short, "data_collector", "==", "0185c7c2eed9d48197953305a817c8b1");
+  # measure.nic2 = subsetDataFrame(measure.df, "data_collector", "==", "0185c7c2eed9d48197953305a817c8b1");
+  # 411f8b5c9500b7cc928c93dd1a0006b7
+  final.df$person_id = trimMe( gsub('"',"",final.df$person_id,fixed=TRUE) );
+  # save final.measure rds
+  saveRDS(final.df,"final.measure.rds");
+  utils::write.table(final.df, file="final.measure.txt", quote=FALSE, col.names=TRUE, row.names=FALSE, sep="|");
+}
